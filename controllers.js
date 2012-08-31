ab45260ca43ded3cfaf7ca3c693f0640224bae0f
Kaat B <kaat.bogaerts@cegeka.be>
2012-08-31 11:11:50 +0200

    kaat: don't add same date twice

--- controllers.js
+++ controllers.js
@@ -3,8 +3,16 @@ function KalenderCtrl($scope, $rootScope) {
 	$rootScope.gekozenDagen = [];
     
 	$scope.addDay = function(dayNumber) {
-   		$rootScope.gekozenDagen.push({dag:dayNumber});
+		if($rootScope.gekozenDagen.some(getJuistGekozenDag, dayNumber)){
+			
+		} else {
+	   		$rootScope.gekozenDagen.push({dag:dayNumber});
+		}
   	};
+  	
+  	function getJuistGekozenDag(value){
+  		return value.dag == this;
+  	}
 }
 
 function AanvraagKiesUrenPerDagCtrl($scope, $rootScope, $routeParams){

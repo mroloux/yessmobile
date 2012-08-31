dde905dfc77478223554b44102486ed2abdeb9d6
unknown <mattir@HI-18264.cegekanv.corp.local>
2012-08-31 13:10:43 +0200

    formatting

--- controllers.js
+++ controllers.js
@@ -16,28 +16,28 @@ function KalenderCtrl($scope, $rootScope) {
 }
 
 geefWekenVanHuidigeMaand = function() {
-  		var weeks = [];
-  		var week = [];
-  		weeks.push(week);
-  		var lastDayOfMonth =  moment().endOf('month');
-  		
-  		var currentDay = moment().startOf('month');
-  		var currentWeek = currentDay.format('w');
-  		
-  		while(currentDay <= lastDayOfMonth) {
-  			var newWeek = currentDay.format('w');
-  			if(newWeek != currentWeek) {
-  				week = [];
-  				weeks.push(week);
-  				currentWeek = newWeek;
-  			}
-  			week.push(currentDay);
-  			currentDay = moment(currentDay);
-  			currentDay.add('days', 1);
-  		}
-  		
-  		return weeks;
-  	}
+	var weeks = [];
+	var week = [];
+	weeks.push(week);
+	var lastDayOfMonth =  moment().endOf('month');
+	
+	var currentDay = moment().startOf('month');
+	var currentWeek = currentDay.format('w');
+	
+	while(currentDay <= lastDayOfMonth) {
+		var newWeek = currentDay.format('w');
+		if(newWeek != currentWeek) {
+			week = [];
+			weeks.push(week);
+			currentWeek = newWeek;
+		}
+		week.push(currentDay);
+		currentDay = moment(currentDay);
+		currentDay.add('days', 1);
+	}
+	
+	return weeks;
+}
 
 function AanvraagKiesUrenPerDagCtrl($scope, $rootScope, $routeParams){
 }

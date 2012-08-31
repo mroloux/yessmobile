e37f834ca19c90356caea2575879083dc5d89a91
unknown <mattir@HI-18264.cegekanv.corp.local>
2012-08-31 11:17:08 +0200

    Fix voor kalendar

--- controllers.js
+++ controllers.js
@@ -5,6 +5,36 @@ function KalenderCtrl($scope, $rootScope) {
 	$scope.addDay = function(dayNumber) {
    		$rootScope.gekozenDagen.push({dag:dayNumber});
   	};
+  	
+  	$scope.wekenVanHuidigeMaand = function() {
+  		var weeks = [];
+  		var week = [];
+  		weeks.push(week);
+  		var lastDayOfMonth =  moment().endOf('month');
+  		
+  		var currentDay = moment().startOf('month');
+  		var currentWeek = currentDay.format('w');
+  		
+  		while(currentDay <= lastDayOfMonth) {
+  			var newWeek = currentDay.format('w');
+  			if(newWeek != currentWeek) {
+  				week = [];
+  				weeks.push(week);
+  				currentWeek = newWeek;
+  			}
+  			week.push(currentDay);
+  			currentDay = moment(currentDay);
+  			currentDay.add('days', 1);
+  		}
+  		
+  		return weeks;
+  	}
+  	
+  	$scope.classVoorWeek = function($isFirst) {
+  		if($isFirst) {
+  			return 'weekdag-row-first';
+  		}
+  	}
 }
 
 function AanvraagKiesUrenPerDagCtrl($scope, $rootScope, $routeParams){

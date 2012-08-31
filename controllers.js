18ca503fb6036a6e1c462a65e50d995dadf802dd
Kaat B <kaat.bogaerts@cegeka.be>
2012-08-31 11:26:56 +0200

    kaat: deselecteren van dag verwijdert dag ook uit scope.gekozenDagen

--- controllers.js
+++ controllers.js
@@ -4,11 +4,15 @@ function KalenderCtrl($scope, $rootScope) {
     
 	$scope.addDay = function(dayNumber) {
 		if($rootScope.gekozenDagen.some(getJuistGekozenDag, dayNumber)){
-			
+			$rootScope.gekozenDagen = $rootScope.gekozenDagen.filter(getAlleAndereDagen, dayNumber);
 		} else {
 	   		$rootScope.gekozenDagen.push({dag:dayNumber});
 		}
   	};
+  	
+  	function getAlleAndereDagen(value){
+  		return value.dag != this;
+  	}
   	
   	function getJuistGekozenDag(value){
   		return value.dag == this;

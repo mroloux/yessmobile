5d53e982aeb9296f425bc56d43d5c3976d64aac6
unknown <mattir@HI-18264.cegekanv.corp.local>
2012-08-31 10:12:10 +0200

    Functie om weken van maand op te halen

--- kalender.js
+++ kalender.js
@@ -5,4 +5,15 @@ function KalenderCtrl($scope) {
 	$scope.addDay = function(dayNumber) {
    		$scope.gekozenDagen.push({dag:dayNumber});
   	};
+  	
+  	$scope.wekenVanHuidigeMaand = function() {
+  		var firstDayOfMonth = moment().startOf('month');
+  		var lastDayOfMonth =  moment().endOf('month');
+  		var currentDay = firstDayOfMonth; 
+  		var days = [];
+  		while(currentDay != lastDayOfMonth) {
+  			days.push(currentDay);
+  		}
+  		return days;
+  	} 
 }
